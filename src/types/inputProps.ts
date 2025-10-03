fa04c45c00ec662234b4c647869da6ef6dede536
KelsonZh0 <kel180498@gmail.com>
2025-10-03 20:12:50 -0300

    feat: atualizar componente Input para suportar registro no formulário e melhorar o tratamento de erros.

--- src/types/inputProps.ts
+++ src/types/inputProps.ts
@@ -1,7 +1,17 @@
+import type { UseFormRegister } from "react-hook-form";
+
+export interface IUserFormValues {
+  nome?: string;
+  nomeUsuario: string;
+  email: string;
+}
+
 export type InputProps = {
   id: string;
+  name: keyof IUserFormValues;
   label: string;
   type?: string;
   placeholder?: string;
+  register: UseFormRegister<IUserFormValues>;
   error?: string;
 };
