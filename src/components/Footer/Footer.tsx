95c51b405a1476260f7b761ee62dbf6d0306a995
KelsonZh0 <kel180498@gmail.com>
2025-10-04 19:40:13 -0300

    feat: adicionando componente Rodape

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+// src/components/Rodape/index.tsx
+export default function Rodape() {
+  return (
+    <footer>
+      <p>
+        &copy; 2025 • Todos os direitos reservados • Controle de Acesso
+        {' '}— Produzido por <strong>UsGuri DEV</strong>
+      </p>
+    </footer>
+  );
+}
