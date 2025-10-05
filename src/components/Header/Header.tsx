7c0d23fb5b24c6664b0f9d06f9e5f8061250ec62
PxS00 <lucas.rossonidd@gmail.com>
2025-10-05 18:34:55 -0300

    refactor: reorganiza lógica de usuário no Header e adiciona gerenciamento de eventos, para mostrar nome e email do usuario

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import type { CadastroSchema } from "../../types/schema";
 
 export default function Header() {
   const navigate = useNavigate();
+  const [usuario, setUsuario] = useState<CadastroSchema | null>(null);
 
   function logout() {
     localStorage.removeItem("usuarioLogado");
@@ -9,13 +12,39 @@ export default function Header() {
     navigate("/");
   }
 
-  const usuario = JSON.parse(localStorage.getItem("usuarioLogado") || "null");
+  useEffect(() => {
+    const getUsuario = () => {
+      try {
+        return JSON.parse(localStorage.getItem("usuarioLogado") || "null");
+      } catch {
+        return null;
+      }
+    };
+
+    setUsuario(getUsuario());
+
+    const handleUserChange = () => {
+      setUsuario(getUsuario());
+    };
+
+    window.addEventListener("userChanged", handleUserChange);
+
+    return () => {
+      window.removeEventListener("userChanged", handleUserChange);
+    };
+  }, []);
 
   return (
     <header>
       <h1>Bem-Vindo ao Sistema de Controle de Acesso</h1>
       {usuario && (
         <div>
+          <p>
+            Usuário: <strong>{usuario.nome}</strong>
+          </p>
+          <p>
+            Email: <strong>{usuario.email}</strong>
+          </p>
           <button onClick={logout}>Sair</button>
         </div>
       )}
