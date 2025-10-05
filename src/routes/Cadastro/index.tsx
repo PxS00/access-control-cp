ccb91671719861256c472f5a57f8dbb48bb7e91c
alex-isidro <alexander.d.isidro@gmail.com>
2025-10-05 10:53:04 -0300

    feat: Adiciona campos com Input

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import type { IUserFormValues } from "../../types/inputProps";
+import Input from "../../components/Form/Input";
 
 const API_URL = "http://localhost:3001/usuarios";
 
@@ -26,6 +27,33 @@ export default function Cadastro() {
 
         <form onSubmit={onSubmit}>
           <fieldset>
+            <Input
+              id="nome"
+              name={"nome"}
+              label="Nome"
+              placeholder="Digite seu nome completo"
+              register={register}
+              error={errors.nome && "Campo obrigatório"}
+            />
+
+            <Input
+              id="nomeUsuario"
+              name={"nomeUsuario"}
+              label="Nome de Usuário"
+              placeholder="Digite seu nome de usuário"
+              register={register}
+              error={errors.nomeUsuario && "Campo obrigatório"}
+            />
+
+            <Input
+              id="email"
+              name={"email"}
+              label="E-mail"
+              placeholder="Digite seu e-mail"
+              register={register}
+              error={errors.email && "Campo obrigatório"}
+            />
+
             <div>
               <button type="submit">Cadastrar</button>
             </div>
